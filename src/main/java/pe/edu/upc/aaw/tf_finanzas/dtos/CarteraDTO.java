74b11351787dc9ff32b1db35e2e7c2229ebec16e
Carlos Vásquez Rodriguez <81259501+Carlou134@users.noreply.github.com>
2024-11-01 19:17:36 -0500

    Cartera CRUD funcional

--- src/main/java/pe/edu/upc/aaw/tf_finanzas/dtos/CarteraDTO.java
+++ src/main/java/pe/edu/upc/aaw/tf_finanzas/dtos/CarteraDTO.java
@@ -1,12 +1,20 @@
 package pe.edu.upc.aaw.tf_finanzas.dtos;
 
+import pe.edu.upc.aaw.tf_finanzas.entities.Bancos;
 import java.time.LocalDate;
-import java.util.Date;
 
 public class CarteraDTO {
     private int id;
-    private String nombre_cartera ;
+    private UserDTO usuarios;
+    private Bancos bancos;
     private LocalDate fecha_descuento;
+    private String moneda;
+    private double total_valor_nominal;
+    private double total_valor_descontado;
+    private double total_descuento;
+    private double tcea;
+    private int dias_promedio;
+    private String estado;
 
     public int getId() {
         return id;
@@ -16,12 +24,20 @@ public class CarteraDTO {
         this.id = id;
     }
 
-    public String getNombre_cartera() {
-        return nombre_cartera;
+    public UserDTO getUsuarios() {
+        return usuarios;
     }
 
-    public void setNombre_cartera(String nombre_cartera) {
-        this.nombre_cartera = nombre_cartera;
+    public void setUsuarios(UserDTO usuarios) {
+        this.usuarios = usuarios;
+    }
+
+    public Bancos getBancos() {
+        return bancos;
+    }
+
+    public void setBancos(Bancos bancos) {
+        this.bancos = bancos;
     }
 
     public LocalDate getFecha_descuento() {
@@ -30,5 +46,61 @@ public class CarteraDTO {
 
     public void setFecha_descuento(LocalDate fecha_descuento) {
         this.fecha_descuento = fecha_descuento;
+    }
+
+    public String getMoneda() {
+        return moneda;
+    }
+
+    public void setMoneda(String moneda) {
+        this.moneda = moneda;
+    }
+
+    public double getTotal_valor_nominal() {
+        return total_valor_nominal;
+    }
+
+    public void setTotal_valor_nominal(double total_valor_nominal) {
+        this.total_valor_nominal = total_valor_nominal;
+    }
+
+    public double getTotal_valor_descontado() {
+        return total_valor_descontado;
+    }
+
+    public void setTotal_valor_descontado(double total_valor_descontado) {
+        this.total_valor_descontado = total_valor_descontado;
+    }
+
+    public double getTotal_descuento() {
+        return total_descuento;
+    }
+
+    public void setTotal_descuento(double total_descuento) {
+        this.total_descuento = total_descuento;
+    }
+
+    public double getTcea() {
+        return tcea;
+    }
+
+    public void setTcea(double tcea) {
+        this.tcea = tcea;
+    }
+
+    public int getDias_promedio() {
+        return dias_promedio;
+    }
+
+    public void setDias_promedio(int dias_promedio) {
+        this.dias_promedio = dias_promedio;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
     }
 }
--- src/main/java/pe/edu/upc/aaw/tf_finanzas/entities/Cartera.java
+++ src/main/java/pe/edu/upc/aaw/tf_finanzas/entities/Cartera.java
@@ -10,18 +10,44 @@ public class Cartera {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    @Column(name = "nombre_cartera", length = 200, nullable = false)
-    private String nombre_cartera ;
+    @ManyToOne
+    @JoinColumn(name = "usuario_id")
+    private Users usuarios;
+    @ManyToOne
+    @JoinColumn(name = "banco_id")
+    private Bancos bancos;
     @Column(name = "fecha_descuento", nullable = false)
     private LocalDate fecha_descuento;
+    @Column(name = "moneda", length = 15, nullable = false)
+    private String moneda;
+    @Column(name = "total_valor_nominal")
+    private double total_valor_nominal;
+    @Column(name = "total_valor_descontado")
+    private double total_valor_descontado;
+    @Column(name = "total_descuento")
+    private double total_descuento;
+    @Column(name = "tcea")
+    private double tcea;
+    @Column(name = "dias_promedio")
+    private int dias_promedio;
+    @Column(name = "estado", length = 20, nullable = false)
+    private String estado;
 
     public Cartera() {
     }
 
-    public Cartera(int id, String nombre_cartera, LocalDate fecha_descuento) {
+    public Cartera(int id, Users usuarios, Bancos bancos, LocalDate fecha_descuento, String moneda, double total_valor_nominal, double total_valor_descontado, double total_descuento, double tcea, int dias_promedio, String estado) {
         this.id = id;
-        this.nombre_cartera = nombre_cartera;
+        this.usuarios = usuarios;
+        this.bancos = bancos;
         this.fecha_descuento = fecha_descuento;
+        this.moneda = moneda;
+        this.total_valor_nominal = total_valor_nominal;
+        this.total_valor_descontado = total_valor_descontado;
+        this.total_descuento = total_descuento;
+        this.tcea = tcea;
+        this.dias_promedio = dias_promedio;
+        this.estado = estado;
     }
 
     public int getId() {
@@ -32,12 +58,20 @@ public class Cartera {
         this.id = id;
     }
 
-    public String getNombre_cartera() {
-        return nombre_cartera;
+    public Users getUsuarios() {
+        return usuarios;
     }
 
-    public void setNombre_cartera(String nombre_cartera) {
-        this.nombre_cartera = nombre_cartera;
+    public void setUsuarios(Users usuarios) {
+        this.usuarios = usuarios;
+    }
+
+    public Bancos getBancos() {
+        return bancos;
+    }
+
+    public void setBancos(Bancos bancos) {
+        this.bancos = bancos;
     }
 
     public LocalDate getFecha_descuento() {
@@ -46,5 +80,61 @@ public class Cartera {
 
     public void setFecha_descuento(LocalDate fecha_descuento) {
         this.fecha_descuento = fecha_descuento;
+    }
+
+    public String getMoneda() {
+        return moneda;
+    }
+
+    public void setMoneda(String moneda) {
+        this.moneda = moneda;
+    }
+
+    public double getTotal_valor_nominal() {
+        return total_valor_nominal;
+    }
+
+    public void setTotal_valor_nominal(double total_valor_nominal) {
+        this.total_valor_nominal = total_valor_nominal;
+    }
+
+    public double getTotal_valor_descontado() {
+        return total_valor_descontado;
+    }
+
+    public void setTotal_valor_descontado(double total_valor_descontado) {
+        this.total_valor_descontado = total_valor_descontado;
+    }
+
+    public double getTotal_descuento() {
+        return total_descuento;
+    }
+
+    public void setTotal_descuento(double total_descuento) {
+        this.total_descuento = total_descuento;
+    }
+
+    public double getTcea() {
+        return tcea;
+    }
+
+    public void setTcea(double tcea) {
+        this.tcea = tcea;
+    }
+
+    public int getDias_promedio() {
+        return dias_promedio;
+    }
+
+    public void setDias_promedio(int dias_promedio) {
+        this.dias_promedio = dias_promedio;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
     }
 }
